e32ba9b528c1c67faa7f97234dc83fb7ee942290
NeptuneRjo <rjo9000@outlook.com>
2026-02-11 16:48:27 -0500

    Added types

--- lib/types.ts
+++ lib/types.ts
@@ -1,5 +1,7 @@
+import { Dispatch, SetStateAction } from "react";
 import * as schema from "./database/schema";
 
+// Use when typing any object that's directly from the database.
 export type Character = typeof schema.characters.$inferSelect;
 export type Wound = typeof schema.wounds.$inferSelect;
 export type Trait = typeof schema.traits.$inferSelect;
@@ -7,15 +9,20 @@ export type Skill = typeof schema.skills.$inferSelect;
 export type Equipment = typeof schema.equipment.$inferSelect;
 export type Stats = typeof schema.stats.$inferSelect;
 export type CharacterSkill = Skill & typeof schema.characterSkills.$inferSelect;
+export type Action = typeof schema.actions.$inferSelect;
+export type Reaction = typeof schema.reactions.$inferSelect;
 
 export type Sheet = Character & {
   traits: Trait[];
-  stats: Stats | {};
+  stats: Stats;
   wounds: Wound[];
   equipment: Equipment[];
   skills: CharacterSkill[];
+  actions: Action[];
+  reactions: Reaction[];
 };
 
+// Use when typing any object that's not directly from the database.
 export type InsCharacter = typeof schema.characters.$inferInsert;
 export type InsWound = typeof schema.wounds.$inferInsert;
 export type InsTrait = typeof schema.traits.$inferInsert;
@@ -23,9 +30,14 @@ export type InsSkill = typeof schema.skills.$inferInsert;
 export type InsEquipment = typeof schema.equipment.$inferInsert;
 export type InsStats = typeof schema.equipment.$inferInsert;
 export type InsCharacterSkill = typeof schema.characterSkills.$inferInsert;
+export type InsAction = typeof schema.actions.$inferInsert;
+export type InsReaction = typeof schema.reactions.$inferInsert;
 
-const physicalBuilds = ["Lithe", "Average", "Hulking"] as const;
-export type PhysicalBuild = (typeof physicalBuilds)[number];
+const physicalBuildTypes = ["Lithe", "Average", "Hulking"] as const;
+
+// Lets us check if a value exists in the physical builds list.
+export const PhysicalBuildTypes = physicalBuildTypes as readonly string[];
+export type PhysicalBuilds = (typeof physicalBuildTypes)[number];
 
 export type BuildModifiers = {
   hitclassBonus: number;
@@ -38,14 +50,16 @@ export type BuildModifiers = {
   force: number;
 };
 
-const damageThresholds = [
+const damageThresholdTypes = [
   "Trivial",
   "Light",
   "Medium",
   "Heavy",
   "Deadly",
 ] as const;
-export type DamageThresholds = (typeof damageThresholds)[number];
+// Lets us check if a value exists in the damage threshold list.
+export const DamageThresholdTypes = damageThresholdTypes as readonly string[];
+export type DamageThresholds = (typeof damageThresholdTypes)[number];
 
 const physicalDamageTypes = [
   "Piercing",
@@ -53,7 +67,10 @@ const physicalDamageTypes = [
   "Bludgeoning",
   "Cleaving",
 ] as const;
-export type PhysicalDamageTypes = (typeof physicalDamageTypes)[number];
+
+// Lets us check if a value exists in the damage type list.
+export const PhysicalDamageTypes = physicalDamageTypes as readonly string[];
+export type PhysicalDamage = (typeof physicalDamageTypes)[number];
 
 export type CurrentEffect = {
   label: string;
@@ -63,4 +80,40 @@ export type CurrentEffect = {
 export type CurrentPenalties = {
   movementPenalty: number;
   statPenalty: number;
+};
+
+export const woundDefinitions = {
+  "Generic Trivial Wound": {
+    tier: "Trivial",
+    severity: 1,
+  },
+  "Generic Light Wound": {
+    tier: "Light",
+    severity: 2,
+  },
+  "Generic Medium Wound": {
+    tier: "Medium",
+    severity: 3,
+  },
+  "Generic Heavy Wound": {
+    tier: "Heavy",
+    severity: 4,
+  },
+  "Bleeding Gash": {
+    tier: "Bleeding",
+    severity: 2,
+  },
+} as const;
+
+export type PostWoundBody = {
+  threshold: string;
+  damageType: string;
+  characterUID: string;
+};
+
+export type SheetContextType = {
+  character: Sheet | null;
+  isLoading: boolean;
+  setCharacter: Dispatch<SetStateAction<Sheet | null>>;
+  getCharacter: (characterUID: string) => Promise<void>;
 };
