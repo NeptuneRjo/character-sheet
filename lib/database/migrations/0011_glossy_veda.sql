fbd7490be6fe3a35f5a28ff381d4af68ccada35c
NeptuneRjo <rjo9000@outlook.com>
2026-02-20 02:23:14 -0500

    Migrations

---
+++ lib/database/migrations/0011_glossy_veda.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "characters" DROP COLUMN "resilience_max";--> statement-breakpoint
+ALTER TABLE "characters" DROP COLUMN "hitclass";--> statement-breakpoint
+ALTER TABLE "characters" DROP COLUMN "movespeed";
