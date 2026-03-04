16dc6a9f602eb3d73c2963401170223d6cf681aa
NeptuneRjo <rjo9000@outlook.com>
2026-03-04 02:36:41 -0500

    Updated name of gm panel provider

--- lib/providers/Providers.tsx
+++ lib/providers/Providers.tsx
@@ -1,6 +1,11 @@
 import { ReactNode } from "react";
 import { SheetProvider } from "./SheetProvider";
+import { GMPanelProvider } from "./GMPanelProvider";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <SheetProvider>{children}</SheetProvider>;
+  return (
+    <GMPanelProvider>
+      <SheetProvider>{children}</SheetProvider>
+    </GMPanelProvider>
+  );
 }
