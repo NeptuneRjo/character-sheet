b2f1cd67beedde11c601c1fdc48dc7d13f09742d
NeptuneRjo <rjo9000@outlook.com>
2026-03-13 02:39:23 -0400

    Created routes for getting equipment/traits/actions/reactions

---
+++ app/api/actions/[[...slug]]/route.ts
@@ -0,0 +1,20 @@
+import { getActions } from "@/lib/database/queries";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ slug?: string[] | undefined }> }
+) {
+  const { slug } = await params;
+
+  try {
+    const response = slug ? [] : await getActions();
+
+    return NextResponse.json(response, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong. ", error: error },
+      { status: 500 }
+    );
+  }
+}
---
+++ app/api/equipment/[[...slug]]/route.ts
@@ -0,0 +1,20 @@
+import { getEquipment } from "@/lib/database/queries";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ slug?: string[] | undefined }> }
+) {
+  const { slug } = await params;
+
+  try {
+    const response = slug ? [] : await getEquipment();
+
+    return NextResponse.json(response, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong. ", error: error },
+      { status: 500 }
+    );
+  }
+}
