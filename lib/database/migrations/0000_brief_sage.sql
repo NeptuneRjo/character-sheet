964770919df1510b8a57ee0e96e6e39e0f2e4bc3
NeptuneRjo <rjo9000@outlook.com>
2026-03-13 19:46:44 -0400

    Migrations

--- lib/database/migrations/0000_brief_sage.sql
+++ lib/database/migrations/0000_brief_sage.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "character_actions" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"character_id" integer NOT NULL,
+	"character_id" uuid NOT NULL,
 	"name" text NOT NULL,
 	"note" text,
 	"cost" smallint DEFAULT 1 NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE "character_actions" (
 --> statement-breakpoint
 CREATE TABLE "character_equipment" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"character_id" integer NOT NULL,
+	"character_id" uuid NOT NULL,
 	"name" text NOT NULL,
 	"description" text NOT NULL,
 	"quantity" smallint DEFAULT 1 NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE "character_equipment" (
 --> statement-breakpoint
 CREATE TABLE "character_reactions" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"character_id" integer NOT NULL,
+	"character_id" uuid NOT NULL,
 	"name" text NOT NULL,
 	"note" text,
 	"cost" smallint DEFAULT 1 NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE "character_reactions" (
 --> statement-breakpoint
 CREATE TABLE "character_skills" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"character_id" integer NOT NULL,
+	"character_id" uuid NOT NULL,
 	"flat_modifier" smallint DEFAULT 2 NOT NULL,
 	"bonus_dice" text DEFAULT '1d4' NOT NULL,
 	"name" text NOT NULL,
@@ -40,14 +40,14 @@ CREATE TABLE "character_skills" (
 --> statement-breakpoint
 CREATE TABLE "character_traits" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"character_id" integer NOT NULL,
+	"character_id" uuid NOT NULL,
 	"name" text NOT NULL,
 	"description" text NOT NULL,
 	"trait_id" text NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "characters" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"resilience_current" smallint DEFAULT 0 NOT NULL,
 	"resilience_reserves" smallint DEFAULT 1 NOT NULL,
 	"action_points" smallint DEFAULT 4 NOT NULL,
@@ -56,8 +56,6 @@ CREATE TABLE "characters" (
 	"baseMoveSpeed" smallint DEFAULT 5 NOT NULL,
 	"name" text DEFAULT 'john' NOT NULL,
 	"isCaster" boolean DEFAULT false NOT NULL,
-	"character_uid" text NOT NULL,
-	CONSTRAINT "characters_character_uid_unique" UNIQUE("character_uid"),
 	CONSTRAINT "ck_phys_build" CHECK ("characters"."physical_build" IN ('Lithe', 'Average', 'Hulking'))
 );
 --> statement-breakpoint
@@ -69,7 +67,7 @@ CREATE TABLE "stats" (
 	"wil" smallint DEFAULT 0 NOT NULL,
 	"acu" smallint DEFAULT 0 NOT NULL,
 	"pre" smallint DEFAULT 0 NOT NULL,
-	"character_id" integer
+	"character_id" uuid
 );
 --> statement-breakpoint
 CREATE TABLE "wounds" (
@@ -77,7 +75,7 @@ CREATE TABLE "wounds" (
 	"name" text NOT NULL,
 	"tier" text DEFAULT 'Trivial' NOT NULL,
 	"severity" smallint DEFAULT 0 NOT NULL,
-	"character_id" integer
+	"character_id" uuid
 );
 --> statement-breakpoint
 ALTER TABLE "character_actions" ADD CONSTRAINT "character_actions_character_id_characters_id_fk" FOREIGN KEY ("character_id") REFERENCES "public"."characters"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
