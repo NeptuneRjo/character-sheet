c992eaafb941f36066a48812f9b02cbb1e501d96
NeptuneRjo <rjo9000@outlook.com>
2026-03-18 15:57:33 -0400

    Migrations

---
+++ lib/database/migrations/0001_mute_dark_beast.sql
@@ -0,0 +1,16 @@
+ALTER TABLE "character_actions" DROP COLUMN "name";--> statement-breakpoint
+ALTER TABLE "character_actions" DROP COLUMN "note";--> statement-breakpoint
+ALTER TABLE "character_actions" DROP COLUMN "cost";--> statement-breakpoint
+ALTER TABLE "character_actions" DROP COLUMN "difficulty";--> statement-breakpoint
+ALTER TABLE "character_equipment" DROP COLUMN "name";--> statement-breakpoint
+ALTER TABLE "character_equipment" DROP COLUMN "description";--> statement-breakpoint
+ALTER TABLE "character_equipment" DROP COLUMN "quantity";--> statement-breakpoint
+ALTER TABLE "character_reactions" DROP COLUMN "name";--> statement-breakpoint
+ALTER TABLE "character_reactions" DROP COLUMN "note";--> statement-breakpoint
+ALTER TABLE "character_reactions" DROP COLUMN "cost";--> statement-breakpoint
+ALTER TABLE "character_reactions" DROP COLUMN "difficulty";--> statement-breakpoint
+ALTER TABLE "character_skills" DROP COLUMN "name";--> statement-breakpoint
+ALTER TABLE "character_skills" DROP COLUMN "ability";--> statement-breakpoint
+ALTER TABLE "character_skills" DROP COLUMN "utility";--> statement-breakpoint
+ALTER TABLE "character_traits" DROP COLUMN "name";--> statement-breakpoint
+ALTER TABLE "character_traits" DROP COLUMN "description";
