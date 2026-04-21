3f7029f4b0c900d6e404a3c918315677c44de89e
NeptuneRjo <rjo9000@outlook.com>
2026-04-21 02:49:33 -0400

    Implemented combat start, end and turn ticking

--- lib/providers/CombatProvider.tsx
+++ lib/providers/CombatProvider.tsx
@@ -9,7 +9,7 @@ const initializationError = (func: string) => {
 
 export const CombatContext = createContext<CombatContextType>({
   currentTurn: 0,
-  currentRound: 0,
+  currentRound: 1,
   combatStart: false,
   combatOrder: [],
   addCombatant: () => initializationError("addCombatant"),
@@ -17,7 +17,10 @@ export const CombatContext = createContext<CombatContextType>({
   updatePlayerOrder: () => initializationError("updatePlayerOrder"),
   updateCombatantOrder: () => initializationError("updateCombatantOrder"),
   handlers: {
-    handleCombatStart: () => initializationError("handleCombatStart"),
+    startCombat: () => initializationError("startCombat"),
+    endCombat: () => initializationError("endCombat"),
+    nextTurn: () => initializationError("nextTurn"),
+    nextRound: () => initializationError("nextRound"),
   },
   setters: {
     setPlayerOrder: () => initializationError("setPlayerOrder"),
@@ -27,6 +30,7 @@ export const CombatContext = createContext<CombatContextType>({
 export const CombatProvider = ({ children }: { children: ReactNode }) => {
   const [combatStart, setCombatStart] = useState<boolean>(false);
 
+  // Tells us who is currently up in initiative.
   const [currentTurn, setCurrentTurn] = useState<number>(0);
   const [currentRound, setCurrentRound] = useState<number>(1);
 
@@ -37,11 +41,12 @@ export const CombatProvider = ({ children }: { children: ReactNode }) => {
 
   const combatOrder = useMemo(() => {
     return [...playerCombatOrder, ...combatantCombatOrder]?.sort(
-      (a, b) => b.turnOrder - a.turnOrder
+      (a, b) => b.initiative - a.initiative
     );
   }, [playerCombatOrder, combatantCombatOrder]);
 
   useEffect(() => {
+    // If the orders are empty we populate them with any data stored in local storage.
     (async () => {
       if (playerCombatOrder.length <= 0) {
         const stored = localStorage.getItem("player-order");
@@ -60,18 +65,34 @@ export const CombatProvider = ({ children }: { children: ReactNode }) => {
         }
       }
     })();
+
+    (async () => {
+      const stored = localStorage.getItem("in-combat");
+      if (stored) {
+        const data = await JSON.parse(stored);
+
+        setCombatStart(data.combatStart);
+        setCurrentRound(data.currentRound);
+        setCurrentTurn(data.currentTurn);
+      }
+    })();
   }, []);
 
   useEffect(() => {
-    if (playerCombatOrder.length > 0) {
+    if (combatStart && playerCombatOrder.length > 0) {
       playerCombatOrder.forEach((sheet) => {
         const channel = supabase.channel(`player:${sheet.character.id}`);
-        channel.send({ type: "broadcast", event: "combat", payload: sheet });
+        channel.send({
+          type: "broadcast",
+          event: "combat-start",
+          payload: sheet,
+        });
       });
     }
-  }, [playerCombatOrder]);
+  }, [combatStart]);
 
   useEffect(() => {
+    // When the orders are updated, update the stored orders (delete if empty).
     if (playerCombatOrder.length > 0) {
       localStorage.setItem("player-order", JSON.stringify(playerCombatOrder));
     } else {
@@ -94,7 +115,7 @@ export const CombatProvider = ({ children }: { children: ReactNode }) => {
     ) {
       // creates our list of players in combat.
       const combatants: CombatSheet[] = sheets.map((sheet) => {
-        return { ...sheet, turnOrder: 0 };
+        return { ...sheet, initiative: 0 };
       });
       setPlayerCombatOrder(combatants);
     }
@@ -110,9 +131,7 @@ export const CombatProvider = ({ children }: { children: ReactNode }) => {
     setPlayerCombatOrder(updatedCombatOrder);
   };
 
-  const handleCombatStart = (playerCombatOrder: CombatSheet[]) => {};
-
-  const addCombatant = (name: string, turnOrder: number) => {
+  const addCombatant = (name: string, initiative: number) => {
     if (name === "") {
       return;
     }
@@ -126,7 +145,7 @@ export const CombatProvider = ({ children }: { children: ReactNode }) => {
       {
         // so we can differentiate between like-named combatants without generating some form of ID.
         name: count > 0 ? `${name} ${count + 1}` : name,
-        turnOrder,
+        initiative,
       },
     ]);
   };
@@ -148,9 +167,100 @@ export const CombatProvider = ({ children }: { children: ReactNode }) => {
     setCombatantCombatOrder(updatedCombatantList);
   };
 
+  const startCombat = () => {
+    setCombatStart(true);
+    setCurrentTurn(0);
+
+    localStorage.setItem(
+      "in-combat",
+      JSON.stringify({
+        combatStart: true,
+        currentRound: 1,
+        currentTurn: 0,
+      })
+    );
+
+    if ("character" in combatOrder[currentTurn]) {
+      alertPlayer(combatOrder[currentTurn], true);
+    }
+  };
+
+  const endCombat = (sheets: Sheet[]) => {
+    setCombatStart(false);
+    setCurrentTurn(0);
+    setCurrentRound(1);
+    setCombatantCombatOrder([]);
+
+    localStorage.removeItem("in-combat");
+
+    sheets.forEach((sheet) => {
+      const channel = supabase.channel(`player:${sheet.character.id}`);
+      channel.send({
+        type: "broadcast",
+        event: "combat-end",
+        payload: sheet,
+      });
+    });
+  };
+
+  const nextTurn = () => {
+    const endOfOrder = currentTurn === combatOrder.length - 1;
+    // If we reach the end of combat order go back to the start and increase the round. Otherwise go next-
+    const turn = endOfOrder ? 0 : currentTurn + 1;
+    const round = endOfOrder ? currentRound + 1 : currentRound;
+
+    const combatant = combatOrder[turn];
+    // If we're at the start of order, use the last index- otherwise use the normal turn value. (Prevents an undefined object).
+    const prevCombatant =
+      combatOrder[turn === 0 ? combatOrder.length - 1 : turn - 1];
+
+    setCurrentTurn(turn);
+    setCurrentRound(round);
+
+    console.log(combatOrder[6]);
+
+    if ("character" in combatant) {
+      alertPlayer(combatant, true);
+    }
+
+    // Alerts the player that their turn is over.
+    if ("character" in prevCombatant) {
+      alertPlayer(prevCombatant, false);
+    }
+  };
+
+  const nextRound = () => {
+    const prevCombatant = combatOrder[currentTurn];
+
+    if ("character" in prevCombatant) {
+      alertPlayer(prevCombatant, false);
+    }
+
+    setCurrentTurn(0);
+    setCurrentRound(currentRound + 1);
+
+    const combatant = combatOrder[0];
+
+    if ("character" in combatant) {
+      alertPlayer(combatant, true);
+    }
+  };
+
+  const alertPlayer = (sheet: CombatSheet, isTurn: boolean) => {
+    const channel = supabase.channel(`player:${sheet.character.id}`);
+    channel.send({
+      type: "broadcast",
+      event: "combat-turn",
+      payload: isTurn,
+    });
+  };
+
   const values = {
     handlers: {
-      handleCombatStart,
+      startCombat,
+      endCombat,
+      nextTurn,
+      nextRound,
     },
     setters: {
       setPlayerOrder,
